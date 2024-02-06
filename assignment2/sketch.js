b3c5a374c1da366b9e32bb5cc0784bb3c16d6ac9
jmayeLSU <jmaye34@lsu.edu>
2024-02-06 06:53:47 -0600

    assignment2 initial

---
+++ assignment2/sketch.js
@@ -0,0 +1,25 @@
+
+
+
+
+function setup() {
+  createCanvas(400, 400);
+  
+  colors = [
+    color('red'), 
+    color('orange'), 
+    color('yellow'), 
+    color('green'),
+    color('cyan'),
+    color('blue'),
+    color('purple'),
+    color('brown'),
+    color('white'),
+    color('black'),
+  ]
+
+}
+
+function draw() {
+  background(220);
+}
